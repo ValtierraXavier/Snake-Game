e04da2835323387335412782c635253e368a1857
ValtierraXavier <xavier.valtierra@icloud.com>
2024-01-06 22:28:56 -0500

    Add better pause mechanism, add status display(play/pause/dead) ignore key inputs when game is not in play.

--- Snake.js
+++ Snake.js
@@ -2,9 +2,12 @@ const canvas = document.getElementById("canvas")
 c = canvas.getContext("2d")
 const canvasSize = document.getElementById('boardSize')
 const unitSize = document.getElementById('unitSize')
+const wallSettings = document.getElementById('wall')
 const settings = document.getElementById('selectionsDiv')
 const counter = document.getElementById('counter')
 const counterSection = document.getElementById('counterSection')
+const pauseDisplay = document.getElementById('pauseDisplay')
+let wallDeath = 'true'
 let boardStart = 0
 let boardSize = Number(canvasSize.value)
 canvas.height = Number(canvasSize.value)
@@ -15,6 +18,7 @@ const maxX = (boardSize)
 const minY = (boardStart)
 const maxY = (boardSize)
 let interval
+let blinker
 const blink = (ms) => {
     return new Promise 
     (resolve => setTimeout(()=>
@@ -35,32 +39,71 @@ const head = {
     dy: Number(unitSize.value),
     length: 0,
     currentKey: 'ArrowDown',
+    paused: false,
     displayPause: true,
+    initial: true,
     isDead: false,
     headColor: "red",
     speed: 150,
     // starts the interval calls to draw the board
     start: () => {
-        getRandomCoordinates()
-        resetBoard()
-        head.displayPause = false 
-        head.isDead = false
-        settings.style.visibility = 'hidden'
+        head.paused = false
+        head.displayPause = false
+        head.initial = false
+        head.isDead = false 
+        food.getRandomCoordinates()
         settings.style.opacity = '0%'
-        interval = setInterval(() => {
-            gameBoard()
+        settings.style.visibility = 'hidden'
+        interval = setInterval(
+            () => {gameBoard()
         },head.speed)
-        canvas.focus()
+        pauseDisplay.innerHTML = 'Playing'
     },
     // clears the draw interval
     stop: () => {
         clearInterval(interval);
-        head.displayPause = true;
     },
+    resume: () => {
+        interval = setInterval(()=>{
+            gameBoard()
+        },head.speed)
+
+    },
+    pause: () => {
+        if(head.paused == false && head.initial == false){
+            head.paused = true
+            head.stop()
+            // pauseDisplay.style.opacity = '100%'
+            pauseDisplay.innerHTML = 'Paused'
+            blinker = setInterval(()=>{
+                if(head.headColor == 'red'){
+                    head.headColor = 'black'
+                    drawBoard()
+                    c.clearRect(head.currentCoordinates.x, head.currentCoordinates.y, head.size, head.size)
+                    head.draw()
+                }else{
+                    head.headColor = 'red'
+                    c.clearRect(head.currentCoordinates.x, head.currentCoordinates.y, head.size, head.size)
+                    drawBoard()
+                    head.draw()
+                }
+            }, 500)
+        }else if(head.paused == true && head.initial == false){
+            head.headColor = 'red'
+            clearInterval(blinker)
+            head.paused = false
+            head.resume()
+            pauseDisplay.innerHTML = 'Playing'
+            // pauseDisplay.style.opacity = '0%'
+        }
+    },
+    
     //resets the board and clears the draw interval
     dead: async () => {
-        head.isDead = true
-        clearInterval(interval)
+        pauseDisplay.innerHTML = "Dead, Awww"
+        head.isDead = true  
+        head.displayPause = true      
+        clearInterval(interval) 
         for(let i = 0; i < 10; i++){
             await blink(250)
             if(head.headColor === "red"){
@@ -81,6 +124,17 @@ const head = {
         c.fillRect(head.currentCoordinates.x, head.currentCoordinates.y, head.size, head.size)
         c.stroke()
     },  
+    checkForFood: () => {
+        if(head.currentCoordinates.x == food.x && head.currentCoordinates.y == food.y){
+            head.length++
+            counter.innerHTML = head.length
+            food.getRandomCoordinates()
+            drawBoard()
+            food.draw()
+            tail.draw
+            head.draw()
+        }
+    }
 }
 
 const tail = {
@@ -107,10 +161,18 @@ const tail = {
                 c.fillRect(tail.tailArr[i-1 >= 0?i-1:i]?.x, tail.tailArr[i-1 >= 0?i-1:i]?.y, head.size, head.size)
                 c.stroke()
             }
+        }  
+    },
+    //checks if head's current position is included in the tail coordinate's array
+    checkForTail: () => {
+    for(let i = 0; i < tail.tailArr.length; i++){
+        if(head.currentCoordinates.x === tail.tailArr[i].x && head.currentCoordinates.y === tail.tailArr[i].y){
+            head.isDead = true
+            head.displayPause = true
+            setTimeout(()=>{head.dead()}, 10)
         }
-        
-    }      
-}
+    }
+}}
 
 const food = {
     // initial coordinates set to a random number within the game board at an interval of the head.size
@@ -122,13 +184,26 @@ const food = {
     dy: head.size,
     displayPause: true,
     foodColor: "yellow",
+    
     draw: () => {
         c.beginPath()
         c.fillStyle = `${food.foodColor}`
         c.fillRect(food.x, food.y , head.size, head.size)
         c.stroke()
-    }
+    },
+
+    getRandomCoordinates: () => {
+        //used to calculate the active board area with relation to the snake head size
+        const divs = boardSize / head.size
+        //Sets random coordinates for food object's next position
+        const randomXMultiplier = Math.floor(Math.random() * divs)
+        const randomYMultiplier = Math.floor(Math.random() * divs)
+        food.x = randomXMultiplier * head.size
+        food.y = randomYMultiplier * head.size
+        food.draw()
+    },
 }
+
 //Draw the board on initial load
 window.addEventListener('DOMContentLoaded',()=>{
     settings.style.opacity = '0%'
@@ -142,27 +217,12 @@ window.addEventListener('DOMContentLoaded',()=>{
     },200)
 })
 
-//detect space bar and toggle game state pause or unpause
-window.addEventListener('keydown',(e)=>{
-    setKey(e)
-    if(e.key === ' '){
-        if(head.displayPause){
-            console.log("unpaused")
-            head.start()
-        }
-        else{
-            console.log("pause")
-            head.stop()
-        }
-    }
-}) 
-
 //handle game functions
 const gameBoard = ()=>{
     //if game is not paused check the coordinates of snake vs food and draw new food if overlapping
     if(!head.displayPause){
-        checkForFood()
-        checkForTail()
+        head.checkForFood()
+        tail.checkForTail()
     }
     //draw black board
     drawBoard()
@@ -170,8 +230,9 @@ const gameBoard = ()=>{
     //if the up arrow key is pressed the snake head's coordinates will be decremented (go up) along the x-axis 
     if(head.currentKey === 'ArrowUp'){
         if(head.currentCoordinates.y === boardStart){
-            head.currentCoordinates.y = boardSize - head.size
-            // head.dead()
+            wallDeath === 'false' ?
+            head.currentCoordinates.y = boardSize - head.size:
+            head.dead()
         }else{
             
             head.currentCoordinates.y -= head.dy
@@ -180,8 +241,9 @@ const gameBoard = ()=>{
     //if the right arrow key is pressed the snake head's coordinates will be incremented (go right) along the x-axis 
     if(head.currentKey === 'ArrowRight'){
         if(head.currentCoordinates.x === boardStart + (boardSize - head.size) ){
-            head.currentCoordinates.x = boardStart
-            // head.dead()
+            wallDeath === 'false' ? 
+            head.currentCoordinates.x = boardStart :
+            head.dead()
         }else{
             head.currentCoordinates.x += head.dx
         }
@@ -189,8 +251,9 @@ const gameBoard = ()=>{
     //if the down arrow key is pressed the snake head's coordinates will be incremented (go down) along the y-axis 
     if(head.currentKey === 'ArrowDown'){
         if(head.currentCoordinates.y === boardStart + (boardSize - head.size)){
-            head.currentCoordinates.y = boardStart
-            // head.dead()
+            wallDeath === 'false' ?
+            head.currentCoordinates.y = boardStart:
+            head.dead()
         }else{
             head.currentCoordinates.y += head.dy
         }
@@ -198,8 +261,9 @@ const gameBoard = ()=>{
     //if the left arrow key is pressed the snake head's coordinates will be decremented (go left) along the x-axis 
     if(head.currentKey === 'ArrowLeft'){
         if(head.currentCoordinates.x === boardStart){
-            head.currentCoordinates.x = boardSize - head.size
-            // head.dead()
+            wallDeath === 'false' ?
+            head.currentCoordinates.x = boardSize - head.size:
+            head.dead()
         }else{
             head.currentCoordinates.x -= head.dx
         }
@@ -228,53 +292,30 @@ const drawBoard = () => {
     c.stroke()
 }
 
-//gets random coordinates to draw food object in a random location on the board
-const getRandomCoordinates = () => {
-    //used to calculate the active board area with relation to the snake head size
-    const divs = boardSize / head.size
-    
-    c.clearRect(food.x, food.y, head.size, head.size)
-    const randomXMultiplier = Math.floor(Math.random() * divs)
-    const randomYMultiplier = Math.floor(Math.random() * divs)
-    food.x = randomXMultiplier * head.size
-    food.y = randomYMultiplier * head.size
-    food.draw()
-}
-
-//checks snake coordinates against the food coordinates
-const checkForFood = () => {
-    if(
-        head.currentCoordinates.x === food.x && head.currentCoordinates.y === food.y
-    ){
-        getRandomCoordinates()
-        head.length++
-        counter.innerHTML = head.length 
-    }
-}
-
-const checkForTail = () => {
-    for(let i = 0; i < tail.tailArr.length; i++){
-        if(head.currentCoordinates.x === tail.tailArr[i].x && head.currentCoordinates.y === tail.tailArr[i].y){
-            head.isDead = true
-            head.displayPause = true
-            setTimeout(()=>{head.dead()}, 10)
-        }
-    }
-}
+//detect space bar and toggle game state pause or unpause
+window.addEventListener('keydown',(e)=>{
+    setKey(e)
+}) 
 
 //sets what arrowkey was press for snake head direction
 const setKey = (key) => {
     if(key.key === ' '){
         key.preventDefault()
-        return
+        if(head.initial == true){
+            head.start()
+        }else{
+            head.pause()
+        }
     }
     if(key.key === 'ArrowUp' || key.key === 'ArrowDown' || key.key === 'ArrowLeft' || key.key === 'ArrowRight'){
         key.preventDefault()
-        head.currentKey = key.key
+        if(head.paused || head.displayPause || head.initial){
+            return
+        }else{
+            head.currentKey = key.key
+        }
     }
 }
-
-
 
 //resets all parameters and draws a fresh board
 const resetBoard = async () => {
@@ -282,15 +323,19 @@ const resetBoard = async () => {
     head.currentCoordinates.y = boardStart
     head.previousCoordinates.x = boardStart
     head.previousCoordinates.y = boardStart
+    head.paused = false
+    head.initial = true
+    head.displayPause = true
     head.currentKey = "ArrowDown"
     head.length = 0
-    head.displayPause = true
     tail.tailArr = []
     counter.innerHTML = 0
     c.clearRect(boardStart, boardStart, boardSize, boardSize)
     drawBoard()
     head.draw()
+    pauseDisplay.innerHTML = 'Press Spacebar to Start'
 }
+//alows useer toselect the board and snake/food sizes. also used to turn of fatal edges (touch the edge and you die)
 const selectSizes = (e)=>{
     let size = Number(e.target.value)
     if(e.target.name === 'boardSize'){        
@@ -304,6 +349,9 @@ const selectSizes = (e)=>{
         head.dy = size
         head.dx = size
         tail.size = size
+    }else if(e.target.name === 'wall'){
+        wallDeath = e.target.value
+
     }
     setTimeout(()=>{
         resetBoard()
@@ -311,3 +359,8 @@ const selectSizes = (e)=>{
 }
 canvasSize.onchange = (e)=>{selectSizes(e)}
 unitSize.onchange = (e)=>{selectSizes(e)}
+wallSettings.onchange = (e)=>{selectSizes(e)}
+
+// next steps add database to save highscores
+// add display for "Paused" with snake head blinking (no food, no tail)
+// add css and title. 
